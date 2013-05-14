571091e8ef336f8a5c4b0351119a469ecb06fe14
Aaron Faanes <dafrito@gmail.com>
2013-05-13 22:19:15 -0500

    Only catch internal Lua exceptions

--- src/ldo.c
+++ src/ldo.c
@@ -52,7 +52,7 @@
 /* C++ exceptions */
 #define LUAI_THROW(L,c)		throw(c)
 #define LUAI_TRY(L,c,a) \
-	try { a } catch(...) { if ((c)->status == 0) (c)->status = -1; }
+	try { a } catch(lua_longjmp* jmp) { if ((c)->status == 0) (c)->status = -1; }
 #define luai_jmpbuf		int  /* dummy variable */
 
 #elif defined(LUA_USE_ULONGJMP)
